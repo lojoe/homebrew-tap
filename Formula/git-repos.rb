26e9e3ddb6bc56a1bf9bc631b29439533b9ee90e
Joe Baldwin <joe@joebaldwin.com>
2017-01-13 07:18:21 -0800

    Fix SHA256 for version git-repos version 0.2.

--- Formula/git-repos.rb
+++ Formula/git-repos.rb
@@ -2,7 +2,7 @@ class GitRepos < Formula
   desc "Git plugin to iterate over multiple repositories"
   homepage "https://github.com/lojoe/git-repos"
   url "https://github.com/lojoe/git-repos/archive/v0.2.tar.gz"
-  sha256 "bddd0bfee1c9d70461679b23de37178f51a5264a45dad571ece87ec2ba144cbc"
+  sha256 "b979d2e94fc50fd781f5d99efd7f88c7f320b23ceee443c93fe38f2c77d1369f"
 
   def install
     bin.install "git-repos"
