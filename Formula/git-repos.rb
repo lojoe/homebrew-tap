4d49dc8c18e8ed5cafa45545905c7148c0225a33
Joe Baldwin <joe@joebaldwin.com>
2017-01-13 07:05:27 -0800

    Update git-repos to v0.2.

--- Formula/git-repos.rb
+++ Formula/git-repos.rb
@@ -1,8 +1,8 @@
 class GitRepos < Formula
   desc "Git plugin to iterate over multiple repositories"
   homepage "https://github.com/lojoe/git-repos"
-  url "https://github.com/lojoe/git-repos/archive/v0.1.tar.gz"
-  sha256 "12c05f5765935396044fa155b1845fa792ebcc9c0a345bbdc5e899ea1517ac50"
+  url "https://github.com/lojoe/git-repos/archive/v0.2.tar.gz"
+  sha256 "bddd0bfee1c9d70461679b23de37178f51a5264a45dad571ece87ec2ba144cbc"
 
   def install
     bin.install "git-repos"
