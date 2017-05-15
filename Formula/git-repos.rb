2038a9b3fd6e96ef67d9cc5b797e9f78832cb0df
Joe Baldwin <joe@joebaldwin.com>
2017-05-15 07:42:27 -0700

    Update git-repos formula to v0.3

--- Formula/git-repos.rb
+++ Formula/git-repos.rb
@@ -1,11 +1,12 @@
 class GitRepos < Formula
-  desc "Git plugin to iterate over multiple repositories"
+  desc "A git plugin to iterate over multiple repositories and quick directory switcher"
   homepage "https://github.com/lojoe/git-repos"
-  url "https://github.com/lojoe/git-repos/archive/v0.2.tar.gz"
-  sha256 "b979d2e94fc50fd781f5d99efd7f88c7f320b23ceee443c93fe38f2c77d1369f"
+  url "https://github.com/lojoe/git-repos/archive/v0.3.tar.gz"
+  sha256 "81190b811ce454ca82fe00040b8eb18b76454f062cc250355ca6030659157feb"
 
   def install
     bin.install "git-repos"
+    bash_completion.install "cr.sh"
   end
 
   test do
